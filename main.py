5f34d40ccd1444f7bd95d88ec07645b22dd9c10f
ryanoconnor <ryan19.roc@gmail.com>
2022-11-15 20:46:29 +0000

    Finish the function and write the logic for the interface in the frontend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly.express as px
+from data_processing import get_data
 
 st.title("Upcoming Weather Forecast")
 
@@ -10,9 +11,18 @@ place = st.text_input("Place: ")
 days = st.slider("Forecast Days",min_value=1,max_value=5,
                  help="Select the number of forecasted days")
 
-# Select box
-select_data = st.selectbox("Select data to view",
-                           ("Temperature","Sky"))
 
 # Sub heading
-st.subheader(f"{select_data} for the next {days} days in {place}")
+st.subheader(f"Temperature for the next {days} days in {place}")
+
+if place:
+    # Call the function from data_processing
+    data = get_data(location=place,forecast_days=days)
+
+    # Get the temperature and sky data to pass to the plot
+    temperatures = [temp['main']['temp']/10 for temp in data]
+    dates = [date["dt_txt"] for date in data]
+
+    # Create a temperature plot using plotly
+    figure = px.line(x=dates,y=temperatures,labels={"x": "Date", "y": "Temperature (C)"})
+    st.plotly_chart(figure)
